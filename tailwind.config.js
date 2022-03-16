815698e76339ef72b374a28596e3d61b2e9c3e9f
Jess Rascal <me@jessrascal.com>
2022-03-16 07:38:27 +0000

    Basics of hero. Custom font added. Other config.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,32 @@
 module.exports = {
   content: ['./src/**/*.{html,js}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Source Sans Pro', 'sans-serif'],
+      },
+      minHeight: {
+        screen80: '80vh',
+        screen85: '85vh',
+        screen90: '90vh',
+      },
+      colors: {
+        outlook: {
+          dark: '#005A9F',
+          light: '#0078D4',
+        },
+        dark: {
+          high: 'rgba(0, 0, 0, 0.87)',
+          medium: 'rgba(0, 0, 0, 0.6)',
+          disabled: 'rgba(0, 0, 0, 0.38)',
+        },
+        light: {
+          high: 'rgba(255, 255, 255, 1)',
+          medium: 'rgba(255, 255, 255, 0.6)',
+          disabled: 'rgba(255, 255, 255, 0.38)',
+        },
+      },
+    },
   },
   plugins: [],
 };
